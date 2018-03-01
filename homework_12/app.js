4e30dc5c07627d5e20d76344ba2cd0ac6bdba86f
andriydzyuba <andriydzyuba91@gmail.com>
2018-03-02 01:42:19 +0200

    done homework_12 (correction)

--- homework_12/app.js
+++ homework_12/app.js
@@ -1,67 +1,135 @@
-var rootNode = document.getElementById("root");
+let rootNode = document.getElementById("root");
 
-var mainNode = document.createElement("div");
+let mainNode = document.createElement("div");
 rootNode.appendChild(mainNode);
 
-var hNode = document.createElement("h1");
+let hNode = document.createElement("h1");
 hNode.innerHTML = "Most popular tanks";
 mainNode.appendChild(hNode);
 
-var boardNode = document.createElement("div");
+let boardNode = document.createElement("div");
 boardNode.classList.add("board");
 mainNode.appendChild(boardNode);
 
 function createNode() {
-  for (var i = 0; i < tanks.length; i++) {
-    var divNode = document.createElement("div");
-    divNode.classList.add("boardTanks");
-    divNode.addEventListener("click", function() {location.hash = tanks.model});
-    boardNode.appendChild(divNode);
 
-    var imgTanks = document.createElement("img");
+  for (let i in tanks) {
+    let a = document.createElement("a");
+    a.setAttribute("href", ('#' + tanks[i].model.replace(/ /gi, "-").toLowerCase()));
+    boardNode.appendChild(a);
+
+    let divNode = document.createElement("div");
+    divNode.classList.add("boardTanks");
+    divNode.setAttribute('title', "Click to details");
+    a.appendChild(divNode);
+
+    let imgTanks = document.createElement("img");
     imgTanks.classList.add("boardimg");
+    imgTanks.setAttribute("title", tanks[i].model);
     imgTanks.setAttribute("src", tanks[i].preview);
     divNode.appendChild(imgTanks);
 
-    var divTanks = document.createElement('div');
+    let divTanks = document.createElement('div');
     divTanks.classList.add("boarddiv");
     divNode.appendChild(divTanks);
 
-    var imgDivTanks = document.createElement("img");
+    let imgDivTanks = document.createElement("img");
     imgDivTanks.classList.add("boardelem");
+    imgDivTanks.setAttribute("title", tanks[i].country);
     imgDivTanks.setAttribute("src", tanks[i].country_image);
     divTanks.appendChild(imgDivTanks);
 
-    var levelDivTanks = document.createElement("p");
+    let levelDivTanks = document.createElement("p");
     levelDivTanks.classList.add("boardelem");
     divTanks.appendChild(levelDivTanks);
     levelDivTanks.append(tanks[i].level);
 
-    var modelDivTanks = document.createElement("p");
+    let modelDivTanks = document.createElement("p");
     modelDivTanks.classList.add("boardelem");
     divTanks.appendChild(modelDivTanks);
     modelDivTanks.append(tanks[i].model.toUpperCase());
   }
 }
 
-createNode(tanks);
+const detailsTank = document.createElement("div");
 
-var detailsNode = document.createElement("div");
-function detailsTank() {
-  
+function detailsNode(){
 
-  var exitDetails = document.createElement("a");
-  exitDetails.innerHTML = "Back to list view";
-  exitDetails.setAttribute("href", "");
-  detailsNode.appendChild(exitDetails);
+  detailsTank.childNodes[0] ? detailsTank.removeChild(detailsTank.childNodes[0]) : null
 
-  rootNode.replaceChild(detailsNode, mainNode);
+  const model = location.hash.slice(1);
+
+  const tankNode = tanks.find((item) => item.model.replace(/ /gi, "-").toLowerCase() === model ? item : null);
+
+  const containerNode = document.createElement("div");
+  detailsTank.appendChild(containerNode);
+
+  const descriptionNode = document.createElement("div");
+  descriptionNode.classList = "description";
+  containerNode.appendChild(descriptionNode);
+
+  const flagTank = document.createElement("img");
+  flagTank.setAttribute("title", tankNode.country);
+  flagTank.setAttribute("src", tankNode.country_image);
+  descriptionNode.appendChild(flagTank);
+
+  const mainTank = document.createElement("h1");
+  mainTank.innerHTML = `${tankNode.model.toUpperCase()} (level ${tankNode.level})`;
+  descriptionNode.appendChild(mainTank);
+
+  const gridTank = document.createElement("div");
+  gridTank.classList = "container-grid";
+  containerNode.appendChild(gridTank);
+
+  const blockTank = document.createElement("div");
+  gridTank.appendChild(blockTank);
+
+  const previewTank = document.createElement("h2");
+  previewTank.innerHTML = "Preview";
+  blockTank.appendChild(previewTank);
+
+  const imgTankNode = document.createElement("img");
+  imgTankNode.setAttribute('src', tankNode.preview);
+  blockTank.appendChild(imgTankNode);
+
+  const backNode = document.createElement("a");
+  backNode.classList = "back";
+  backNode.innerHTML = "Back to list view";
+  backNode.setAttribute("href", "");
+  blockTank.appendChild(backNode);
+
+  const table = document.createElement("table");
+
+  const caption = document.createElement("caption");
+  caption.innerHTML = "Characteristic";
+  table.appendChild(caption);
+
+  for (let prop in tankNode.details){
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
+
+    const tdProp = document.createElement("td");
+    tdProp.innerHTML = prop.replace(/_/gi, " ");
+    tr.appendChild(tdProp);
+
+    const tdValue = document.createElement("td");
+    tdValue.innerHTML = tankNode.details[prop];
+    tr.appendChild(tdValue);
+  }
+  gridTank.appendChild(table);
+
+  rootNode.replaceChild(detailsTank, mainNode);
 }
 
-window.addEventListener('hashchange', function() {
-  if (location.hash) {
-    detailsTank()
-  } else {
-    rootNode.replaceChild(mainNode, detailsNode)
+!location.hash ? createNode(tanks) : detailsNode()
+
+window.addEventListener('hashchange',
+  () => {
+  	if(location.hash){
+      detailsNode();
+    } else {
+      !boardNode.childNodes[0] ? createNode(tanks) : false
+      rootNode.replaceChild(mainNode, detailsTank);
+    }
   }
-});
+);
